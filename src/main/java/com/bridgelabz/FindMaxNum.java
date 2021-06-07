e9310a41ae4575174de8047c71e7b6aa13bd1974
dada4747 <adsurerahul96@gmail.com>
2021-06-07 23:48:03 +0530

    ADDED:Test case for maximum float number

--- src/main/java/com/bridgelabz/FindMaxNum.java
+++ src/main/java/com/bridgelabz/FindMaxNum.java
@@ -2,8 +2,19 @@ package com.bridgelabz;
 
 public class FindMaxNum {
 
-    public int findMaxNumber(Integer num1, Integer num2, Integer num3) {
+    public int findMaxIntNumber(Integer num1, Integer num2, Integer num3) {
         Integer maxNumber = num1;
+        if (num2.compareTo(maxNumber) > 0){
+            maxNumber = num2;
+        }
+        if (num3.compareTo(maxNumber) > 0){
+            maxNumber = num3;
+        }
+        return maxNumber;
+    }
+
+    public float findMaxFloatNumber(Float num1, Float num2, Float num3) {
+        Float maxNumber = num1;
         if (num2.compareTo(maxNumber) > 0){
             maxNumber = num2;
         }
--- src/test/java/com/bridgelabz/FindMaxNumTest.java
+++ src/test/java/com/bridgelabz/FindMaxNumTest.java
@@ -8,18 +8,34 @@ public class FindMaxNumTest {
     FindMaxNum findMaxNum = new FindMaxNum();
     @Test
     void givenThreeIntegerNumber_shouldReturn_firstNumberIsMaximum(){
-        int result = findMaxNum.findMaxNumber(100,50,30);
+        int result = findMaxNum.findMaxIntNumber(100,50,30);
         Assertions.assertEquals(100, result);
     }
 
     @Test
     void givenThreeIntegerNumber_shouldReturn_secondNumberIsMaximum(){
-        int result = findMaxNum.findMaxNumber(50,100,30);
+        int result = findMaxNum.findMaxIntNumber(50,100,30);
         Assertions.assertEquals(100, result);
     }
     @Test
     void givenThreeIntegerNumber_shouldReturn_thirdNumberIsMaximum(){
-        int result = findMaxNum.findMaxNumber(30,50,100);
+        int result = findMaxNum.findMaxIntNumber(30,50,100);
         Assertions.assertEquals(100, result);
+    }
+    @Test
+    void givenThreeFloatNumber_shouldReturn_firstNumberIsMaximum(){
+        float result = findMaxNum.findMaxFloatNumber(10.0F, 5.0F, 3.0F);
+        Assertions.assertEquals(10.0, result);
+    }
+
+    @Test
+    void givenThreeFloatNumber_shouldReturn_secondNumberIsMaximum(){
+        float result = findMaxNum.findMaxFloatNumber(5.0F, 10.0F,3.0F);
+        Assertions.assertEquals(10.0, result);
+    }
+    @Test
+    void givenThreeFloatNumber_shouldReturn_thirdNumberIsMaximum(){
+        float result = findMaxNum.findMaxFloatNumber(3.0F, 5.0F, 10.0F);
+        Assertions.assertEquals(10.0, result);
     }
 }
