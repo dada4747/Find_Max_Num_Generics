ae33e912bca7973c3725bb989682235ac58adadb
dada4747 <adsurerahul96@gmail.com>
2021-06-08 08:42:25 +0530

    REFACTORED:To generic class to take vaiable of genetic type

--- src/main/java/com/bridgelabz/FindMaxNum.java
+++ src/main/java/com/bridgelabz/FindMaxNum.java
@@ -1,6 +1,27 @@
 package com.bridgelabz;
 
-public class FindMaxNum {
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class FindMaxNum<E extends  Comparable<E>> {
+    E value1;
+    E value2;
+    E value3;
+
+    public FindMaxNum(E value1, E value2, E value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public FindMaxNum() {
+
+    }
+    public E findMax(){
+        return findMax(value1, value2, value3);
+    }
 
     public <E extends Comparable<E>> E findMax(E value1, E value2, E value3) {
         E maxNumber = value1;
--- src/test/java/com/bridgelabz/FindMaxNumTest.java
+++ src/test/java/com/bridgelabz/FindMaxNumTest.java
@@ -8,50 +8,60 @@ public class FindMaxNumTest {
     FindMaxNum findMaxNum = new FindMaxNum();
     @Test
     void givenThreeIntegerNumber_shouldReturn_firstNumberIsMaximum(){
-        int result = findMaxNum.findMax(100,50,30);
+        FindMaxNum<Integer> findMaxNum = new FindMaxNum<>(100, 50, 25);
+        int result = findMaxNum.findMax();
         Assertions.assertEquals(100, result);
     }
 
+
     @Test
     void givenThreeIntegerNumber_shouldReturn_secondNumberIsMaximum(){
-        int result = findMaxNum.findMax(50,100,30);
+        FindMaxNum<Integer> findMaxNum = new FindMaxNum<>(50, 100, 25);
+        int result = findMaxNum.findMax();
         Assertions.assertEquals(100, result);
     }
     @Test
     void givenThreeIntegerNumber_shouldReturn_thirdNumberIsMaximum(){
-        int result = findMaxNum.findMax(30,50,100);
+        FindMaxNum<Integer> findMaxNum = new FindMaxNum<>(50, 25, 100);
+        int result = findMaxNum.findMax();
         Assertions.assertEquals(100, result);
     }
     @Test
     void givenThreeFloatNumber_shouldReturn_firstNumberIsMaximum(){
-        float result = findMaxNum.findMax(10.0F, 5.0F, 3.0F);
+        FindMaxNum<Float> findMaxNum = new FindMaxNum<>(10.0F, 5.0F, 3.0F);
+        float result = findMaxNum.findMax();
         Assertions.assertEquals(10.0, result);
     }
 
     @Test
     void givenThreeFloatNumber_shouldReturn_secondNumberIsMaximum(){
-        float result = findMaxNum.findMax(5.0F, 10.0F,3.0F);
+        FindMaxNum<Float> findMaxNum = new FindMaxNum<>(5.0F, 10.0F,3.0F);
+        float result = findMaxNum.findMax();
         Assertions.assertEquals(10.0, result);
     }
     @Test
     void givenThreeFloatNumber_shouldReturn_thirdNumberIsMaximum(){
-        float result = findMaxNum.findMax(3.0F, 5.0F, 10.0F);
+        FindMaxNum<Float> findMaxNum = new FindMaxNum<>(3.0F, 5.0F, 10.0F);
+        float result = findMaxNum.findMax();
         Assertions.assertEquals(10.0, result);
     }
     @Test
     void givenThreeString_shouldReturn_firstStringIsMaximum(){
-        String result = findMaxNum.findMax("peach","apple","banana");
+        FindMaxNum<String> findMaxNum = new FindMaxNum<>("peach","apple","banana");
+        String result = findMaxNum.findMax();
         Assertions.assertEquals("peach", result);
     }
 
     @Test
     void givenThreeString_shouldReturn_secondStringIsMaximum(){
-        String result = findMaxNum.findMax("apple", "peach","banana");
+        FindMaxNum<String> findMaxNum = new FindMaxNum<>("apple", "peach","banana");
+        String result = findMaxNum.findMax();
         Assertions.assertEquals("peach", result);
     }
     @Test
     void givenThreeString_shouldReturn_thirdStringIsMaximum(){
-        String result = findMaxNum.findMax("apple", "banana", "peach");
+        FindMaxNum<String> findMaxNum = new FindMaxNum<>("apple", "banana", "peach");
+        String result = findMaxNum.findMax();
         Assertions.assertEquals("peach", result);
     }
 
